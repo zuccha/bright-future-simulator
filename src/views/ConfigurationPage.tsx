9055efdc388a0dc303070674ffa7f279ac34833a
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2023-04-20 12:35:08 +0200

    feat: add validation on run configuration

--- src/views/ConfigurationPage.tsx
+++ src/views/ConfigurationPage.tsx
@@ -5,12 +5,13 @@ import {
   Flex,
   FormControl,
   FormLabel,
+  Heading,
   Input,
   Switch,
   Text,
   VStack,
 } from "@hope-ui/solid";
-import { createSignal } from "solid-js";
+import { For, Show, createSignal } from "solid-js";
 
 const GAP = 10;
 const FORM_CONTROL_PROPS = { flex: 1, minWidth: 120 };
@@ -22,6 +23,11 @@ const computeNumberOfCircles = (cardsCount: number): number => {
 };
 
 function ConfigurationPage() {
+  const [errors, setErrors] = createSignal<string[]>([]);
+
+  const [isRunningSimulation, setIsRunningSimulation] = createSignal(false);
+  const [simulationProgress, setSimulationProgress] = createSignal(0);
+
   const [playersCount, setPlayersCount] = createSignal(1);
   const [cardsCount, setCardsCount] = createSignal(1);
   const [useCustomMaxCircles, setUseCustomMaxCircles] = createSignal(false);
@@ -52,6 +58,40 @@ function ConfigurationPage() {
     if (!target.checked) setMaxCircles(computeNumberOfCircles(cardsCount()));
   };
 
+  const validate = (): string[] => {
+    const errors: string[] = [];
+    if (playersCount() <= 0) errors.push("Not enough players (min 1)");
+    if (cardsCount() < playersCount())
+      errors.push("Not enough cards in deck for all players");
+    if (maxCircles() < computeNumberOfCircles(cardsCount()))
+      errors.push("Not enought circles, not all cards can be played");
+    return errors;
+  };
+
+  const handleRunSimulation = async () => {
+    const newErrors = validate();
+    if (newErrors.length > 0) {
+      setErrors(newErrors);
+      return;
+    }
+
+    setErrors([]);
+    setIsRunningSimulation(true);
+
+    setSimulationProgress(0);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    setSimulationProgress(0.1);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    setSimulationProgress(0.5);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    setSimulationProgress(0.75);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    setSimulationProgress(0.99);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    setIsRunningSimulation(false);
+  };
+
   return (
     <Flex
       height="100%"
@@ -60,8 +100,16 @@ function ConfigurationPage() {
       padding="$10"
     >
       <Flex maxWidth={300} width="100%" gap={GAP} direction="column">
+        <Heading size="lg" marginBottom="$4">
+          Configure Simulation
+        </Heading>
+
         <Flex wrap="wrap" flex={1} gap={GAP}>
-          <FormControl {...FORM_CONTROL_PROPS} required>
+          <FormControl
+            {...FORM_CONTROL_PROPS}
+            disabled={isRunningSimulation()}
+            required
+          >
             <FormLabel>Players</FormLabel>
             <Input
               min={1}
@@ -75,7 +123,11 @@ function ConfigurationPage() {
         </Flex>
 
         <Flex wrap="wrap" flex={1} gap={GAP}>
-          <FormControl {...FORM_CONTROL_PROPS} required>
+          <FormControl
+            {...FORM_CONTROL_PROPS}
+            disabled={isRunningSimulation()}
+            required
+          >
             <FormLabel>Deck size</FormLabel>
             <Input
               min={1}
@@ -89,7 +141,7 @@ function ConfigurationPage() {
 
           <FormControl
             {...FORM_CONTROL_PROPS}
-            disabled={!useCustomMaxCircles()}
+            disabled={!useCustomMaxCircles() || isRunningSimulation()}
             readOnly={!useCustomMaxCircles()}
             required
           >
@@ -106,13 +158,34 @@ function ConfigurationPage() {
 
           <Checkbox
             checked={useCustomMaxCircles()}
+            disabled={isRunningSimulation()}
             onChange={handleChangeUseCustomMaxCircles}
           >
             Custom number of circles
           </Checkbox>
         </Flex>
 
-        <Button>Run Simulation</Button>
+        <Button
+          disabled={isRunningSimulation()}
+          loading={isRunningSimulation()}
+          loadingText={`${Math.ceil(simulationProgress() * 100)}%`}
+          marginTop="$4"
+          onClick={handleRunSimulation}
+        >
+          Run Simulation
+        </Button>
+
+        <Show when={errors().length > 0}>
+          <Flex direction="column">
+            <For each={errors()}>
+              {(error) => (
+                <Text fontSize="$sm" color="red">
+                  {error}
+                </Text>
+              )}
+            </For>
+          </Flex>
+        </Show>
       </Flex>
     </Flex>
   );
