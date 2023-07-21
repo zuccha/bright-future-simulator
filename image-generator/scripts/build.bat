60b93abb3f30f7bd99ae30f413434223ccfdcaeb
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2023-07-21 15:38:00 +0200

    build: automatically resolve prompts

--- image-generator/scripts/build.bat
+++ image-generator/scripts/build.bat
@@ -1,11 +1,11 @@
 SET outdir=.\dist\image_generator
 
-if EXIST %outdir% RMDIR %outdir%
+if EXIST %outdir% RMDIR %outdir% /s /q
 
 call deno compile --allow-net --allow-read --allow-write --output %outdir%\image_generator main.ts
 
 
-XCOPY README.txt %outdir% /i
+echo D | XCOPY README.txt %outdir% /i
 XCOPY config.json %outdir% /i
 XCOPY list.csv %outdir% /i
 XCOPY images %outdir%\images /e /i
